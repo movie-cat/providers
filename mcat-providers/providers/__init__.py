0967325a50d16b822d880be98ae23fc83390df92
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-08-07 14:41:32 +0100

    Add providers

---
+++ mcat-providers/providers/__init__.py
@@ -0,0 +1,80 @@
+import re
+import hashlib
+from pathlib import Path
+from typing import Optional, Union, List, Dict
+from providers.types import ProviderHeaders, Stream
+
+class Provider:
+    URL_PATTERN = re.compile(r"^(https:\/\/.+)$")
+    TARGET_PATTERNS = {
+        "bandwith": re.compile(r"BANDWIDTH=(\d+)"),
+        "quality": re.compile(r"RESOLUTION=(\d+x\d+)"),
+        "codecs": re.compile(r"CODECS=(?:\"|\')([^\"\']+)"),
+        "uri": re.compile(r"URI=(?:\"|\')([^\'\"]+)")
+    }
+
+    @staticmethod
+    def validate_working_dir(working_dir: Union[Path, str]) -> Path:
+        working_dir = working_dir if isinstance(working_dir, Path) else Path(working_dir)
+        return working_dir if working_dir.is_dir() else working_dir.parent
+
+    @staticmethod
+    def calculate_md5(input_bytes: bytes, _mode: str = "digest") -> bytes:
+        md5 = hashlib.md5(input_bytes)
+        if _mode == "digest":
+            return md5.digest()
+        elif _mode == "hexdigest":
+            return md5.hexdigest()
+        else:
+            raise ValueError("Unknown md5 formatting mode '{}'".format(_mode))
+
+    @classmethod
+    def meta_item(cls, key: str) -> Optional[str]:
+        return cls.__meta__.get(key)
+
+    @classmethod
+    def parse_m3u8(cls, headers: ProviderHeaders, m3u8_url: str, m3u8_data: str) -> List:
+        '''This is probably badly written.'''
+        m3u8_data = m3u8_data.strip().split("\n")
+        m3u8_data_parsed = []
+        if not m3u8_data:
+            print("No m3u8 data!")
+            raise ValueError("No m3u8 data!")
+        expect_url = False
+        parsed_data: Dict[str, str] = {
+            "provider": cls.__name__,
+            "headers": headers,
+            "ext": ".m3u8"
+        }
+        for item in m3u8_data:
+            if expect_url:
+                url_match = Provider.URL_PATTERN.search(item)
+                if not url_match:
+                    continue
+                parsed_data.update({"url": url_match.group(1)})
+                m3u8_data_parsed.append(Stream(**parsed_data))
+                parsed_data: Dict[str, str] = {
+                    "provider": cls.__name__,
+                    "headers": headers,
+                    "ext": ".m3u8"
+                }
+                expect_url = False
+                continue
+            data = {}
+            for target, pattern in Provider.TARGET_PATTERNS.items():
+                regex_match = pattern.search(item)
+                if regex_match:
+                    data.update({target: regex_match.group(1)})
+            if data.get("uri"):
+                uri = data.pop("uri")
+                if not uri.startswith("/"): uri = f"/{uri}"
+                url = f"{m3u8_url}{uri}"
+                data.update({"url": url})
+                parsed_data.update(data)
+                m3u8_data_parsed.append(Stream(**parsed_data))
+                parsed_data: Dict[str, str] = {"ext": ".m3u8"}
+                continue
+            if data:
+                expect_url = True
+                parsed_data.update(data)
+        return m3u8_data_parsed
---
+++ mcat-providers/providers/types.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+
+import re
+from enum import Enum
+from typing import Optional, List, Dict
+
+class Quality(Enum):
+    P_144 = "144p"
+    P_240 = "240p"
+    P_360 = "360p"
+    P_480 = "480p"
+    P_720 = "720p"
+    P_1080 = "1080p"
+    P_1440 = "1440p"
+    P_2160 = "2160p"
+    P_4320 = "4320p"
+    UNKNOWN = "UNKNOWN"
+
+    @staticmethod
+    def process_quality_str(quality_str: str) -> str:
+        quality_str = quality_str.lower()
+        quality_str = quality_str.strip()
+        quality_str = re.sub(r"[^a-zA-Z0-9]", "", quality_str)
+        return quality_str
+
+    @classmethod
+    def map_quality(cls, quality: str) -> Quality:
+        quality = cls.process_quality_str(quality)
+        if quality in ["7680x4320", "4320p", "8k"]:
+            return cls.P_4320
+        elif quality in ["4096x2160", "2160p", "ultrahd", "uhd", "4k"]:
+            return cls.P_2160
+        elif quality in ["2560x1440", "1440p", "quadhd", "wqhd", "qhd"]:
+            return cls.P_1440
+        elif quality in ["1920x1080", "1080p", "fullhd", "fhd"]:
+            return cls.P_1080
+        elif quality in ["1280x720", "720p", "hd"]:
+            return cls.P_720
+        elif quality in ["854x480", "480p"]:
+            return cls.P_480
+        elif quality in ["640x360", "360p"]:
+            return cls.P_360
+        elif quality in ["426x240", "240p"]:
+            return cls.P_240
+        elif quality in ["144p"]:
+            return cls.P_144
+        else:
+            print(f"Unknown quality: {quality}")
+            return cls.UNKNOWN
+
+class ProviderHeaders:
+    def __init__(
+        self, 
+        origin: Optional[str] = None, 
+        referrer: Optional[str] = None, 
+        user_agent: Optional[str] = None, 
+        additional_headers: Optional[Dict] = None,
+        **kwargs
+    ) -> None:
+        self._origin = origin
+        self._referrer = referrer
+        self._user_agent = user_agent
+        self._headers = {
+            "Origin": self._origin,
+            "Referrer": self._referrer,
+            "User-Agent": self._user_agent,
+        }
+        if additional_headers:
+            self._headers.update(additional_headers)
+        self._headers.update(kwargs)
+
+    @property
+    def origin(self) -> str:
+        if not self._origin:
+            raise ValueError("Origin must be set first!")
+        return self._origin
+
+    @origin.setter
+    def origin(self, value: str) -> None:
+        self._origin = value
+        self._headers.update({"Origin": self._origin})
+
+    @property
+    def referrer(self) -> str:
+        if not self._referrer:
+            raise ValueError("Referrer must be set first!")
+        return self._referrer
+
+    @referrer.setter
+    def referrer(self, value: str) -> None:
+        self._referrer = value
+        self._headers.update({"Referrer": self._referrer})
+
+    @property
+    def user_agent(self) -> str:
+        if not self._user_agent:
+            raise ValueError("User agent must be set first!")
+        return self._user_agent
+    
+    @user_agent.setter
+    def user_agent(self, value: str) -> None:
+        self._user_agent = value
+        self._headers.update({"User-Agent": self._user_agent})
+
+    @property
+    def headers(self) -> Dict:
+        if not self._origin:
+            raise ValueError("Origin must be set first!")
+        if not self._referrer:
+            raise ValueError("Referrer must be set first!")
+        if not self._user_agent:
+            raise ValueError("User agent must be set first!")
+        return self._headers.copy()
+
+    def add_header(self, key: str, value: str) -> None:
+        self._headers[key] = value
+
+    def remove_header(self, key: str) -> None:
+        if key in self._headers:
+            del self._headers[key]
+
+    def __repr__(self) -> str:
+        return f"ProviderHeaders(origin='{self._origin}', referrer='{self._referrer}', user_agent='{self._user_agent}'"
+
+class Subtitle:
+    def __init__(self, language: str, url: str, ext: str):
+        self.language = language
+        self.url = url
+        self.ext = ext
+
+    @property
+    def as_dict(self) -> Dict:
+        return {
+            "language": self.language,
+            "url": self.url,
+            "ext": self.ext
+        }
+
+    def __repr__(self) -> str:
+        return f"Subtitle(language='{self.language}', url='{self.url}', ext='{self.ext}')"
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Subtitle):
+            return False
+        return self.language == other.language and self.url == other.url and self.ext == other.ext
+
+    def __hash__(self) -> int:
+        return hash((self.language, self.url, self.ext))
+
+class Stream:
+    def __init__(
+        self, 
+        provider: str,
+        headers: ProviderHeaders,
+        url: str,
+        ext: str,
+        quality: Union[str, Quality], 
+        codec: Optional[str] = None, 
+        bandwith: Optional[int] = None,
+        audio_channels: Optional[int] = None,
+        **kwargs
+    ) -> None:
+        self.provider = provider
+        self.headers = headers
+        self.url = url
+        self.ext = ext
+        self.quality = quality if isinstance(quality, Quality) else Quality.map_quality(quality) 
+        self.codec = codec
+        self.bandwith = bandwith
+        self.audio_channels = audio_channels # TODO
+
+    @property
+    def as_dict(self) -> Dict:
+        return {
+            "provider": self.provider,
+            "headers": self.headers,
+            "url": self.url,
+            "ext": self.ext,
+            "quality": self.quality,
+            "codec": self.codec,
+            "bandwith": self.bandwith,
+            "audio_channels": self.audio_channels
+        }
+
+    def __repr__(self) -> str:
+        return f"Stream(provider='{self.provider}', headers={self.headers}, url='{self.url}', ext='{self.ext}'," \
+               f"quality={self.quality}, codec='{self.codec}', bandwith={self.bandwith}, audio_channels={self.audio_channels})"
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Stream):
+            return False
+        return self.url == other.url and self.quality == other.quality and \
+               self.provider == other.provider and self.headers == other.headers and\
+               self.ext == other.ext and self.codec == other.codec and self.bandwith == other.bandwith and\
+               self.audio_channels == other.audio_channels
+
+    def __hash__(self) -> int:
+        return hash((self.url, self.quality, self.provider, self.headers, self.ext, self.codec, self.bandwith, self.audio_channels))
+
+class ProviderResponse:
+    def __init__(
+            self,
+            provider: str,
+            streams: List[Stream], 
+            subtitles: List[Subtitle],
+        ) -> None:
+        self.provider = provider
+        self.streams = streams
+        self.subtitles = subtitles
+
+    @property
+    def as_dict(self) -> dict:
+        return {
+            "provider": self.provider,
+            "streams": [stream.as_dict for stream in self.streams],
+            "subtitles": [subtitle.as_dict for subtitle in self.subtitles]
+        }
+
+    def __repr__(self) -> str:
+        return f"ProviderResponse(provider='{self.provider}', streams={self.streams}, subtitles={self.subtitles})"
