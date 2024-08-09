39c9bfb9fc301fee33f6dcdc6245a6f1b0477bb2
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-08-09 17:55:09 +0100

    feat: Expand exceptions.py

--- mcat_providers/utils/exceptions.py
+++ mcat_providers/utils/exceptions.py
@@ -1,2 +1,11 @@
 class IntegrityError(ValueError):
     '''Generic Error for instances such as a checksum mismatch when hashing files.'''
+
+class DisabledError(ValueError):
+    '''Generic Error for disabled providers/sources'''
+
+class DisabledProviderError(DisabledError):
+    '''Generic Error for disabled provider'''
+
+class DisabledSourceError(DisabledError):
+    '''Generic Error for disabled source'''
