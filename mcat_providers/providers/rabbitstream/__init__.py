c9f75ffcc337658a2d8209bf1f0e116c8b72a664
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-08-09 17:53:51 +0100

    Lots of small changes

--- mcat_providers/providers/rabbitstream/__init__.py
+++ mcat_providers/providers/rabbitstream/__init__.py
@@ -2,35 +2,35 @@ import re
 import json
 import httpx
 import base64
-import asyncio
 import pythonmonkey
 
-from pathlib import Path
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import unpad
+
+from pathlib import Path
 from typing import Optional, Dict, List
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, RetryError
 
-from mcat_providers.providers import Provider
-from mcat_providers.providers.types import ProviderHeaders, ProviderResponse, Subtitle
-
+from mcat_providers.providers import BaseProvider
 from mcat_providers.utils.exceptions import IntegrityError
+from mcat_providers.utils.types import ProviderHeaders, ProviderResponse, Subtitle
 from mcat_providers.utils.decorators import async_lru_cache, async_lru_cache_parameterless
 
-class Rabbitstream(Provider):
-    __meta__ = {
-        "name": "Rabbitstream",
-        "filename": "payload.js",
-        "file_hash": "d587be31e78f245f63afd5331430d0d1",
-        "file_url": "https://raw.githubusercontent.com/movie-cat/embed-scripts/main/rabbitstream/payload.js",
-        "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0",
-        "referrer": "https://rabbitstream.net",
-        "origin": "https://rabbitstream.net",
+class Rabbitstream(BaseProvider):
+    name = "Rabbitstream"
+    base = "https://rabbitstream.net"
+    default_headers = {
+        "Referer": "https://flixhq.to/",
+        **BaseProvider.default_headers
+    }
+    embedded_file = {
+        "name": "payload.js",
+        "hash": "d587be31e78f245f63afd5331430d0d1",
+        "url": "https://raw.githubusercontent.com/movie-cat/embed-scripts/main/rabbitstream/payload.js"
     }
 
     def __init__(self, **kwargs) -> None:
-        filename = self.meta_item("filename")
-        timeout = httpx.Timeout(kwargs.get("timeout", 999))
+        filename = self.embedded_file["name"]
         working_dir = self.validate_working_dir(kwargs.get("working_dir", Path(__file__)))
         file_dir = working_dir.joinpath(filename)
 
@@ -41,17 +41,18 @@ class Rabbitstream(Provider):
         # If updating doesnt fix the issue then you can fix this manually by updating the file_hash in __meta__ to the new MD5 hash
         # Only update the file hash if you are happy with the content of the file and have deemed it as safe
         if not file_dir.exists():
-            payload_url = self.meta_item("file_url")
-            expected_hash = self.meta_item("file_hash")
+            payload_url = self.embedded_file["url"]
+            expected_hash = self.embedded_file["hash"]
             print(f"Attempting to download most recent version of '{filename}'")
             try:
                 req = httpx.get(payload_url)
-            except httpx.ConnectError:
-                raise IntegrityError("Failed to retrieve '{}'".format(filename))
+            except Exception as e:
+                print(e)
+                raise IntegrityError(f"Failed to retrieve '{filename}'")
             md5 = self.calculate_md5(req.text.encode(), "hexdigest")
             print(f"Checksum = {md5}, Expected = {expected_hash}")
             if md5 != expected_hash:
-                raise IntegrityError("Could not validate the checksum of '{}'...".format(filename))
+                raise IntegrityError(f"Could not validate the checksum of '{filename}'...")
             with open(file_dir, "w", encoding="utf-8") as f:
                 f.write(req.text)
 
@@ -59,12 +60,12 @@ class Rabbitstream(Provider):
             PAYLOAD = f.read()
 
         if not PAYLOAD:
-            raise IntegrityError("Could not find '{}' for the WASM bundle!".format(filename))
+            raise IntegrityError(f"Could not find any content inside '{filename}' for the WASM bundle!")
 
         self.instantiate_and_decrypt = pythonmonkey.eval(PAYLOAD)
-        self.client = httpx.AsyncClient(timeout=timeout)
-        self.client_headers = {"User-Agent": self.meta_item("user-agent"), "Referrer": "https://flixhq.to/"}
-
+        self.client_headers = kwargs.get("headers") or {}
+        self.client_headers.update(self.default_headers)
+    
     @staticmethod
     def base64_to_bytearray(encoded_str) -> bytearray:
         return bytearray(base64.b64decode(encoded_str))
@@ -119,7 +120,7 @@ class Rabbitstream(Provider):
             return None
         return meta_match.group(1)
 
-    async def get_sources(self, xrax: str, kversion: str, kid: str, browserid: str) -> Optional[Dict]:
+    async def get_sources(self, xrax: str, keys: List, kversion: str, kid: str, browserid: str) -> Optional[Dict]:
         req = await self.client.get(
                 f"https://rabbitstream.net/ajax/v2/embed-4/getSources",
                 params={"id": xrax, "v": kversion, "h": kid, "b": browserid},
@@ -127,8 +128,8 @@ class Rabbitstream(Provider):
             )
         if not req.is_success:
             print(
-                "Failed to fetch 'getSources' endpoint!\n\t" /
-                f"Keys: {keys}\n\tKversion: {kversion}\n\tKid: {kid}\n\tBrowserid: {browserid}" / 
+                "Failed to fetch 'getSources' endpoint!\n\t" \
+                f"Keys: {keys}\n\tKversion: {kversion}\n\tKid: {kid}\n\tBrowserid: {browserid}" \
                 f"Url: {req.url}\n\tStatus: {req.status_code}\n\tBody:\n{req.text}"
             )
             return None
@@ -143,20 +144,24 @@ class Rabbitstream(Provider):
         wasm_req = await self.client.get(f'https://rabbitstream.net/images/loading.png?v=0.6', headers=self.client_headers)
         return wasm_req.content
 
-    @async_lru_cache(maxsize=512)
+    @async_lru_cache(maxsize=128)
     @retry(retry=retry_if_exception_type(ValueError), stop=stop_after_attempt(3))
-    async def get_data(self, xrax: str) -> List:
+    async def get_data(self, xrax: str) -> Dict:
         wasm = await self.get_wasm()
         meta = await self.get_meta(xrax)
         if not wasm or not meta:
             raise ValueError("Failed to retrieve wasm or meta!\n\tWasm Exists: {}\nMeta - {}".format(not not wasm, meta))
 
         keys, kversion, kid, browserid = await self.instantiate_and_decrypt(xrax, meta, wasm)
-        sources_data = await self.get_sources(xrax=xrax, kversion=kversion, kid=kid, browserid=browserid)
-        ciphertext = sources_data.pop("sources")
-        if not ciphertext:
+        sources_data = await self.get_sources(xrax=xrax, keys=keys.tolist(), kversion=kversion, kid=kid, browserid=browserid)
+        if not sources_data:
             print("Could not retrieve encrypted sources!")
             raise ValueError("Could not retrieve encrypted sources!")
+
+        ciphertext = sources_data.pop("sources")
+        if not ciphertext:
+            print("Could not retrieve ciphertext from encrypted sources!")
+            raise ValueError("Could not retrieve ciphertext from encrypted sources!")
 
         formatted_key = self.format_wasm_key(
             keys=keys.tolist(),
@@ -186,7 +191,7 @@ class Rabbitstream(Provider):
         sources_data.update({"sources": json.loads(decrypted)})
         return sources_data
 
-    @async_lru_cache(maxsize=512)
+    @async_lru_cache(maxsize=128)
     async def get_qualities(self, playlist: str, provider_headers=ProviderHeaders) -> List:
         req = await self.client.get(playlist, headers=provider_headers.headers)
         if not req.is_success:
@@ -200,7 +205,12 @@ class Rabbitstream(Provider):
             raise ValueError("No result from parse_m3u8!")
         return m3u8_data
 
-    async def resolve(self, xrax: str) -> Optional[ProviderResponse]:
+    async def resolve(self, url: str) -> Optional[ProviderResponse]:
+        # if self.disabled:
+        #     return None
+
+        xrax = url.rpartition("?")[0].rpartition("/")[2]
+
         try:
             data = await self.get_data(xrax)
         except RetryError:
@@ -208,12 +218,11 @@ class Rabbitstream(Provider):
             return None
         except Exception as e:
             print(e)
-            return none
+            return None
 
         headers = ProviderHeaders(
-            origin=self.meta_item("origin"),
-            referrer=self.meta_item("referrer"),
-            user_agent=self.meta_item("user-agent")
+            origin=self.base,
+            referrer=self.base
         )
 
         playlist = data.get("sources")[0]
