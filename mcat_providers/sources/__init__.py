e4d8e348cbf55817cfba02fd60c27f70116f7cc4
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-08-09 17:54:18 +0100

    feat: Add BaseSource

--- mcat_providers/sources/__init__.py
+++ mcat_providers/sources/__init__.py
@@ -1,2 +1,26 @@
-class Source:
-    pass
+from typing import Optional, Dict
+
+from mcat_providers import client, sync_client, default_ua
+from mcat_providers.providers import BaseProvider
+from mcat_providers.utils.types import MediaType
+from mcat_providers.utils.decorators import async_lru_cache
+from mcat_providers.utils.exceptions import DisabledSourceError
+
+class BaseSource:
+    # Variables
+    name: str
+    base: str
+
+    # Defaults
+    client = client
+    sync_client = sync_client
+    default_headers: Dict = {"User-Agent": default_ua}
+
+    # def __init__(self):
+    #     if self.disabled:
+    #         raise DisabledSourceError(f"'{self.__class__.__name__}' has been disabled!")
+
+    @classmethod
+    async def resolve_tmdb(cls, media: MediaType):
+        req = await cls.client.get(f"https://api.dmdb.network/v1/gmid/{media.gmid}")
+        return req.json()
