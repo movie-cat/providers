089ffae2685129e8e321a8b4041e535a6aba20a8
Ciarands <74070993+Ciarands@users.noreply.github.com>
2024-08-10 10:30:26 +0100

    seperate log behaviour based on level

--- mcat_providers/__init__.py
+++ mcat_providers/__init__.py
@@ -1,14 +1,17 @@
+import json
 import httpx
 import asyncio
 import logging
 from rich.logging import RichHandler
 
-FORMAT = "%(message)s"
+fh = logging.FileHandler('debug.log')
 logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    level="NOTSET",
+    format='%(asctime)s %(levelname)s | %(name)s | %(message)s',
+    datefmt='[%H:%M:%S]',
+    handlers=[fh]
 )
-log = logging.getLogger("rich")
-
+log = logging.getLogger()
 loop = asyncio.get_event_loop()
 default_timeout = httpx.Timeout(999)
 default_ua = "Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0"
@@ -18,21 +21,17 @@ sync_client = httpx.Client(timeout=default_timeout)
 
 def main():
     from mcat_providers.sources import flixhq
+    
+    rich_handler = RichHandler(rich_tracebacks=True)
+    rich_handler.setLevel(logging.ERROR)
+    log.addHandler(rich_handler)
 
     source = flixhq.FlixHq()
     sources_list = loop.run_until_complete(
         source.scrape_all(
-            tmdb="4935",
+            tmdb="278",
             media_type="movie",
         )
     )
 
-    log.info(sources_list)
-
-    # first_source = sources_list[0]
-    # log.info(first_source)
-
-    # source = first_source.streams[0]
-    # english_subs = [sub for sub in first_source.subtitles if "english" in sub.language.lower()]
-    # command = f"mpv \"{source.url}\" --referrer=\"{source.headers.referrer}\" --user-agent=\"{source.headers.user_agent}\" --sub-file=\"{english_subs[0].url}\""
-    # log.info(command)
+    return json.dumps(sources_list.as_dict)
